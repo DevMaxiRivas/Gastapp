b4dbb418644dad9408b4afb5d362c110119c681e
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-06-01 11:17:49 -0300

    feat(component): Icon have been added to component QuickReferenceCard

--- src/components/shared/cards/QuickReferenceCard.tsx
+++ src/components/shared/cards/QuickReferenceCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { COLORS_TEXT } from "@/lib/constants-front";
 import { type LucideIcon } from "lucide-react";
 
 type QuickReferenceCardParams = {
@@ -6,28 +7,19 @@ type QuickReferenceCardParams = {
     value: string;
     footer: string;
     type: "success" | "danger" | "neutral";
+    iconCard?: LucideIcon;
 }
 
-const getClassNameByType = (type: "success" | "danger" | "neutral") => {
-    switch (type) {
-        case "success":
-            return "text-green-600";
-        case "danger":
-            return "text-red-600";
-        case "neutral":
-            return "text-blue-600";
-    }
-}
-
-export function QuickReferenceCard({ title, value, footer, type }: QuickReferenceCardParams) {
+export function QuickReferenceCard({ title, value, footer, type, iconCard: IconCard }: QuickReferenceCardParams) {
     return (
         <Card className="@container/card">
-            <CardHeader>
-                <CardDescription>{title}</CardDescription>
-                <CardTitle className={`text-2xl font-semibold tabular-nums @[250px]/card:text-3xl ${getClassNameByType(type)}`}>
-                    {value}
-                </CardTitle>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">{title}</CardTitle>
+                {IconCard && <IconCard className={`h-4 w-4 ${COLORS_TEXT[type]}`} />}
             </CardHeader>
+            <CardContent className={`text-2xl font-semibold tabular-nums @[250px]/card:text-3xl ${COLORS_TEXT[type]}`}>
+                {value}
+            </CardContent>
             <CardFooter className="flex-col items-start gap-1.5 text-sm">
                 <div className="line-clamp-1 flex gap-2 font-medium">
                     {footer}
