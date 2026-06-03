05b7883e81cbac7a7e402cc3c59df3deec07fd0f
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-06-03 11:07:02 -0300

    fix(component): Description param has become optional in SectionAsHeader component

--- src/components/shared/sections/SectionAsCardHeader.tsx
+++ src/components/shared/sections/SectionAsCardHeader.tsx
@@ -2,7 +2,7 @@ import { CardDescription, CardTitle } from "@/components/ui/card";
 
 type CardHeaderProps = {
     title: string;
-    description: string;
+    description?: string;
 }
 export function SectionAsCardHeader({ title, description }: CardHeaderProps) {
     return (
@@ -12,7 +12,7 @@ export function SectionAsCardHeader({ title, description }: CardHeaderProps) {
                     {title}
                 </b>
             </CardTitle>
-            <CardDescription>{description}</CardDescription>
+            {description && <CardDescription>{description}</CardDescription>}
         </>
     );
-} 1
+}
