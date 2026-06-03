fcd615b4d979eb3dab81345081909769be44c4d2
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-06-03 11:40:45 -0300

    fix(page): Color text have been fixed to dark mode

--- src/pages/error/NotFoundPage.tsx
+++ src/pages/error/NotFoundPage.tsx
@@ -9,8 +9,8 @@ export function NotFoundPage() {
                 <div className="h-full flex flex-col items-center justify-center text-sm max-md:px-4">
                     <h1 className="text-8xl md:text-9xl font-bold text-primary">404</h1>
                     <div className="h-1 w-16 rounded bg-primary my-5 md:my-7"></div>
-                    <p className="text-2xl md:text-3xl font-bold text-gray-800">Page Not Found</p>
-                    <p className="text-sm md:text-base mt-4 text-gray-500 max-w-md text-center">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+                    <p className="text-2xl md:text-3xl font-bold text-gray-800 dark:text-gray-200">Page Not Found</p>
+                    <p className="text-sm md:text-base mt-4 text-gray-500 dark:text-gray-400 max-w-md text-center">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
                     <div className="flex items-center gap-4 mt-6">
                         <Link to="/">
                             <Button>Return Home</Button>
