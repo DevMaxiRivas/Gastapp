27498fd002e7a1650a5c5bf102809f7a77a39520
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-07-23 18:04:52 -0300

    fix(component): Styles and Date Format have been updated in TransactionItem

--- src/features/transactions/components/TransactionItem.tsx
+++ src/features/transactions/components/TransactionItem.tsx
@@ -10,6 +10,7 @@ import { TransactionTypeObject } from "@/enums/transaction/TransactionType";
 import { CATEGORY_ICONS, COLORS_BG, COLORS_TEXT } from "@/lib/constantsFront";
 import type { Transaction } from "@/types/backend/transaction/response";
 import { capitalizeFirstLetter } from "@/utils/stringUtils";
+import { format, parse } from "date-fns";
 import type { LucideIcon } from "lucide-react";
 
 export function TransactionItem({ transaction }: { transaction: Transaction }) {
@@ -21,6 +22,8 @@ export function TransactionItem({ transaction }: { transaction: Transaction }) {
 
     const colorType: "danger" | "success" = isExpense ? "danger" : "success";
 
+    const date: string = format(parse(transaction.transactionDate, "yyyy-MM-dd", new Date()), 'd MMMM');
+
     return (
         <Item>
             <ItemMedia>
@@ -31,10 +34,12 @@ export function TransactionItem({ transaction }: { transaction: Transaction }) {
                 </div>
             </ItemMedia>
             <ItemContent className="gap-1">
-                <div className="flex items-center justify-between">
+                <div className="flex flex-col sm:flex-row w-full justify-between">
                     <div>
-                        <ItemTitle>{capitalizeFirstLetter(transaction.category.name)} - {new Date(transaction.transactionDate).toLocaleDateString()}</ItemTitle>
-                        <ItemDescription>{transaction.note ? transaction.note.substring(0, 40) : "-"}</ItemDescription>
+                        <ItemTitle>{capitalizeFirstLetter(transaction.category.name)} - {date}</ItemTitle>
+                        <ItemDescription className="hidden md:block">
+                            {transaction.note ? transaction.note.substring(0, 40) : "-"}
+                        </ItemDescription>
                     </div>
                     <span className={`${COLORS_TEXT[colorType]}`}>
                         {user?.profile?.currency} {(isExpense ? -transaction.amount : transaction.amount).toLocaleString()}
