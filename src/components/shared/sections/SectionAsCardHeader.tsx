f49aeb8826ff6825c9b937acd2daae6f5b345e0a
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-07-23 18:02:00 -0300

    feat(component): Icon prop have been added to SectionAsCardHeader

--- src/components/shared/sections/SectionAsCardHeader.tsx
+++ src/components/shared/sections/SectionAsCardHeader.tsx
@@ -3,16 +3,20 @@ import { CardDescription, CardTitle } from "@/components/ui/card";
 type CardHeaderProps = {
     title: string;
     description?: string;
+    icon?: React.ReactNode;
 }
-export function SectionAsCardHeader({ title, description }: CardHeaderProps) {
+export function SectionAsCardHeader({ title, description, icon }: CardHeaderProps) {
     return (
-        <>
-            <CardTitle className="text-xl font-semibold">
-                <b>
-                    {title}
-                </b>
-            </CardTitle>
-            {description && <CardDescription>{description}</CardDescription>}
-        </>
+        <div className="flex items-center justify-between">
+            <div>
+                <CardTitle className="text-xl font-semibold">
+                    <div className="flex flex-row justify-start items-center gap-3">
+                        {icon}
+                        <b>{title}</b>
+                    </div>
+                </CardTitle>
+                {description && <CardDescription>{description}</CardDescription>}
+            </div>
+        </div>
     );
 }
